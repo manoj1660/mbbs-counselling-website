0bb18679cdf52be9cbf4c7255bf626b5ce2fb65b
saquib39 <mohdsaquib619@gmail.com>
2026-03-15 15:37:48 +0530

    video changd

--- src/components/HeroVideoDialogDemoTopInBottomOut.jsx
+++ src/components/HeroVideoDialogDemoTopInBottomOut.jsx
@@ -6,7 +6,7 @@ export function HeroVideoDialogDemoTopInBottomOut() {
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/TyW9HVcG2sI?autoplay=1&playsinline=1&rel=0"
+        videoSrc="https://www.youtube.com/embed/-fREY9S7ze0"
         thumbnailSrc="/images/hero-video.png"
         thumbnailAlt="Hero Video"
       />
@@ -14,7 +14,7 @@ export function HeroVideoDialogDemoTopInBottomOut() {
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/embed/TyW9HVcG2sI?autoplay=1&playsinline=1&rel=0"
+        videoSrc="https://www.youtube.com/embed/-fREY9S7ze0"
         thumbnailSrc="/images/hero-video.png"
         thumbnailAlt="Hero Video"
       />
